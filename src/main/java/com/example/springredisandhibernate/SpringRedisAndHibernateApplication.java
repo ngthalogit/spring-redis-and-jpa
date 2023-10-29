9b66779d4167c786a2aecee89c62b67ee93ece61
ngthalogit <ngthlo.lap@MacBook-Pro-cua-Nguyen.local>
2023-10-29 18:38:05 +0700

    project initialization

---
+++ src/main/java/com/example/springredisandhibernate/SpringRedisAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springredisandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisAndHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisAndHibernateApplication.class, args);
+    }
+
+}
